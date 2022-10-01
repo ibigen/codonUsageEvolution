604afec91ccfd7cb267bdae91d5e668be5a21d60
Francisca Almeida <francisca.2.2.2000@gmail.com>
2022-10-01 19:39:14 +0100

    [add] correction and addition of the code from tests

--- src/tests/test_tables.py
+++ src/tests/test_tables.py
@@ -5,8 +5,6 @@ Created on 17/09/2022
 '''
 import unittest
 import os
-
-
 from codon_usage_evolution import read_genome, save_table
 import filecmp
 from utils.utils import Utils
@@ -14,6 +12,7 @@ from utils.utils import Utils
 
 class Test(unittest.TestCase):
     utils = Utils()
+
     def setUp(self):
         self.baseDirectory = os.path.dirname(os.path.abspath(__file__))
 
@@ -32,22 +31,27 @@ class Test(unittest.TestCase):
         ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
         self.assertTrue(os.path.exists(ecoli_fasta))
 
-        ## call method
+        # call method
         dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
 
-        ## not finished, test if this gene is inside data frame
-        self.assertTrue(dataframe_counts['lcl|NC_000913.3_cds_NP_414542.1_1'] in dataframe_counts)
-        self.assertEqual(1, dataframe_counts['lcl|NC_000913.3_cds_NP_414542.1_1']['AAA'])
+
+        # test if this gene is inside data frame
+        self.assertIn("lcl|NC_000913.3_cds_NP_414542.1_1", dataframe_counts.index)
+        self.assertEqual(1, dataframe_counts['AAA']['lcl|NC_000913.3_cds_NP_414542.1_1'])
 
     def test_tables(self):
         ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
-        expected_result = os.path.join(self.baseDirectory, "files/tables/result_temp.csv")
-        self.assertTrue(os.path.exists(expected_result))
+        expected_result_RSCU_CAI = os.path.join(self.baseDirectory, "files/tables/table_RSCU_CAI_ecoli.csv")
+        self.assertTrue(os.path.exists(expected_result_RSCU_CAI))
         dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
-        csv_result = self.utils.get_temp_file("abc", ".csv")
-        save_table(dataframe_counts, csv_result)
-        self.assertTrue(filecmp.cmp(expected_result, csv_result))
+        csv_result_RSCU_CAI = self.utils.get_temp_file("RSCU_and_CAI_to_test", ".csv")
+        save_table(dataframe_RSCU_CAI, csv_result_RSCU_CAI)
+        self.assertTrue(filecmp.cmp(expected_result_RSCU_CAI, csv_result_RSCU_CAI))
 
+        expected_result_counts = os.path.join(self.baseDirectory, "files/tables/table_counts_ecoli.csv")
+        csv_result_counts = self.utils.get_temp_file("counts_to_test", ".csv")
+        save_table(dataframe_counts, csv_result_counts)
+        self.assertTrue(filecmp.cmp(expected_result_counts, csv_result_counts))
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.test_read_fasta']
