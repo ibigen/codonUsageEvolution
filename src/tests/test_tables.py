d2719e5d0785cb5e89b13b4ea14f92addfd808c9
mmp <monsanto@ua.pt>
2022-10-03 16:13:29 +0100

    [fix] touch in windows

--- src/tests/test_tables.py
+++ src/tests/test_tables.py
@@ -11,48 +11,53 @@ from utils.utils import Utils
 
 
 class Test(unittest.TestCase):
-    utils = Utils()
+	utils = Utils()
 
-    def setUp(self):
-        self.baseDirectory = os.path.dirname(os.path.abspath(__file__))
+	def setUp(self):
+		self.baseDirectory = os.path.dirname(os.path.abspath(__file__))
 
-    def tearDown(self):
-        pass
+	def tearDown(self):
+		pass
 
-    def sum(self, a, b):
-        """ sum two numbers, only for example"""
-        return a + b
+	def sum(self, a, b):
+		""" sum two numbers, only for example"""
+		return a + b
 
-    def test_sum(self):
-        self.assertEqual(5, self.sum(3, 2))
-        self.assertNotEqual(6, self.sum(3, 2))
+	def test_sum(self):
+		self.assertEqual(5, self.sum(3, 2))
+		self.assertNotEqual(6, self.sum(3, 2))
 
-    def test_read_fasta(self):
-        ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
-        self.assertTrue(os.path.exists(ecoli_fasta))
+	def test_read_fasta(self):
+		ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
+		self.assertTrue(os.path.exists(ecoli_fasta))
 
-        # call method
-        dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
+		# call method
+		dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
 
 
-        # test if this gene is inside data frame
-        self.assertIn("lcl|NC_000913.3_cds_NP_414542.1_1", dataframe_counts.index)
-        self.assertEqual(1, dataframe_counts['AAA']['lcl|NC_000913.3_cds_NP_414542.1_1'])
+		# test if this gene is inside data frame
+		self.assertIn("lcl|NC_000913.3_cds_NP_414542.1_1", dataframe_counts.index)
+		self.assertEqual(1, dataframe_counts['AAA']['lcl|NC_000913.3_cds_NP_414542.1_1'])
 
-    def test_tables(self):
-        ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
-        expected_result_RSCU_CAI = os.path.join(self.baseDirectory, "files/tables/table_RSCU_CAI_ecoli.csv")
-        self.assertTrue(os.path.exists(expected_result_RSCU_CAI))
-        dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
-        csv_result_RSCU_CAI = self.utils.get_temp_file("RSCU_and_CAI_to_test", ".csv")
-        save_table(dataframe_RSCU_CAI, csv_result_RSCU_CAI)
-        self.assertTrue(filecmp.cmp(expected_result_RSCU_CAI, csv_result_RSCU_CAI))
+	def test_tables(self):
+		ecoli_fasta = os.path.join(self.baseDirectory, "files/references/ecoli.fasta")
+		expected_result_RSCU_CAI = os.path.join(self.baseDirectory, "files/tables/table_RSCU_CAI_ecoli.csv")
+		self.assertTrue(os.path.exists(expected_result_RSCU_CAI))
+		dataframe_counts, dataframe_RSCU_CAI, dataframe_CAI = read_genome(ecoli_fasta)
+		csv_result_RSCU_CAI = self.utils.get_temp_file("RSCU_and_CAI_to_test", ".csv")
+		save_table(dataframe_RSCU_CAI, csv_result_RSCU_CAI)
+		print(expected_result_RSCU_CAI, csv_result_RSCU_CAI)
+		self.assertTrue(filecmp.cmp(expected_result_RSCU_CAI, csv_result_RSCU_CAI))
 
-        expected_result_counts = os.path.join(self.baseDirectory, "files/tables/table_counts_ecoli.csv")
-        csv_result_counts = self.utils.get_temp_file("counts_to_test", ".csv")
-        save_table(dataframe_counts, csv_result_counts)
-        self.assertTrue(filecmp.cmp(expected_result_counts, csv_result_counts))
+		expected_result_counts = os.path.join(self.baseDirectory, "files/tables/table_counts_ecoli.csv")
+		csv_result_counts = self.utils.get_temp_file("counts_to_test", ".csv")
+		save_table(dataframe_counts, csv_result_counts)
+		self.assertTrue(filecmp.cmp(expected_result_counts, csv_result_counts))
 
+		## remove temp files
+		self.utils.remove_file(csv_result_RSCU_CAI)
+		self.utils.remove_file(csv_result_counts)
+		
 if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.test_read_fasta']
-    unittest.main()
+	# import sys;sys.argv = ['', 'Test.test_read_fasta']
+	unittest.main()
